a07464108a603cec5789143ac636aebbf69b7257
aurelo <zlatko.gudasic@gmail.com>
2023-08-22 09:11:39 +0200

    added pathname to redirect url

--- src/app/auth/auth-config.module.ts
+++ src/app/auth/auth-config.module.ts
@@ -8,8 +8,8 @@ import { AuthModule } from 'angular-auth-oidc-client';
               configId: 'google',
               authority: 'https://accounts.google.com',
               // logout url: https://www.google.com/accounts/logout
-              redirectUrl: window.location.origin,
-              postLogoutRedirectUri: window.location.origin,
+              redirectUrl: window.location.origin + window.location.pathname,
+              postLogoutRedirectUri: window.location.origin  + window.location.pathname,
               clientId: '491499355097-0v04u87qvu9b1ei4kmmff3hu8jv3gk2t.apps.googleusercontent.com',
               scope: 'openid profile email', // 'openid profile offline_access ' + your scopes
               responseType: 'id_token',
